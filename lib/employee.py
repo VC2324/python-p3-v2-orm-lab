d73f4ce8bda6d9eacf57e074e4a4fc0feefec2c9
Victor <victorac2324@gmail.com>
2024-05-04 23:13:03 -0400

    used Object relational mapping to do a one to many "

--- lib/employee.py
+++ lib/employee.py
@@ -18,6 +18,8 @@ class Employee:
             f"<Employee {self.id}: {self.name}, {self.job_title}, " +
             f"Department ID: {self.department_id}>"
         )
+    
+    
 
     @property
     def name(self):
@@ -186,5 +188,16 @@ class Employee:
         return cls.instance_from_db(row) if row else None
 
     def reviews(self):
+        from review import Review
+        sql='''
+            SELECT * FROM reviews
+            WHERE employee_id = ?
+            '''
+        CURSOR.execute(sql,(self.id,),)
+
+        rows = CURSOR.fetchall()
+        return[
+            Review.instance_from_db(row) for row in rows
+        ]
         """Return list of reviews associated with current employee"""
         pass
